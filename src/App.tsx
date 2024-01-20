2f293e3f49fc33c0c6fd0a6f53652b286dccda84
Yanis Ahmed <yanisahmed12@gmail.com>
2024-01-20 21:10:42 +0600

    React Redux added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import {
+  decrement,
+  increment,
+  reset,
+} from "./redux/features/counter/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-      <div className="h-16">0</div>
+      <div className="h-16">{count}</div>
       <div className="flex">
-        <button className="bg-sky-500 hover:bg-sky-700 text-white rounded-2xl p-2 w-48 me-2">
+        <button
+          onClick={() => dispatch(increment())}
+          className="bg-sky-500 hover:bg-sky-700 text-white rounded-2xl p-2 w-48 me-2"
+        >
           Increment
         </button>
-        <button className="bg-green-500 hover:bg-green-700 text-white rounded-2xl p-2 w-48 me-2">
+        <button
+          onClick={() => dispatch(decrement())}
+          className="bg-green-500 hover:bg-green-700 text-white rounded-2xl p-2 w-48 me-2"
+        >
           Decrement
         </button>
-        <button className="bg-red-500 hover:bg-red-700 text-white rounded-2xl p-2 w-48">
-          Decrement
+        <button
+          onClick={() => dispatch(reset())}
+          className="bg-red-500 hover:bg-red-700 text-white rounded-2xl p-2 w-48"
+        >
+          Reset
         </button>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import store from "./redux/store.tsx";
 import router from "./routes/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </Provider>
   </React.StrictMode>
 );
